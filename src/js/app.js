3a4b03977e8a8e00701f45a7eb20a7732237b3f7
Youri Pols <youri@MBP-van-Youri.fritz.box>
2022-09-16 16:41:19 +0200

    Story block

--- src/js/app.js
+++ src/js/app.js
@@ -9,3 +9,28 @@ window.addEventListener("scroll", () => {
         nav.classList.remove("scrolled");
     }
 });
+
+function counter() {
+    $(".amount").appear();
+    $(".amount").on("appear", function () {
+        $(this).each(function () {
+            this.initNumAnim = true;
+            var $this = $(this),
+                countTo = $this.attr("data-amount");
+            $({ countNum: $this.text() }).animate(
+                { countNum: countTo },
+                {
+                    duration: 1000,
+                    step: function () {
+                        $this.text(Math.floor(this.countNum));
+                    },
+                    complete: function () {
+                        $this.text(this.countNum);
+                    },
+                }
+            );
+        });
+    });
+}
+
+counter();
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,5 @@
+import "../../node_modules/jquery/dist/jquery.min.js";
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
+import "./appear.js";
 import "./app";
